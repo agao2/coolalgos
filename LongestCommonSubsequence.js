1f729b5362b3f7b51e343f06bd5bd04f29e67dbf
Alex <18474012+agao2@users.noreply.github.com>
2020-02-03 23:33:26 -0700

    Update LongestCommonSubsequence.js

--- LongestCommonSubsequence.js
+++ LongestCommonSubsequence.js
@@ -1,20 +1,28 @@
 
 
-
+// Dynamic programming! O(n^2) time complexity
 const longestCommonSubsequence = (text1, text2) => {
     let matrix = [];
     for (let i = 0; i < text1.length; i++) {
         matrix.push(new Array(text2.length).fill(0))
     }
-    
+
+    for (let i = 0; i < text1.length; i++) {
+        for (let j = 0; j < text2.length; j++) {
+            if (text1[i] === text2[j]) {
+                matrix[i][j] = (i === 0 || j === 0) ? 1 : (matrix[i - 1][j - 1] + 1)
+            } else {
+                let length1 = (i === 0) ? 0 : (matrix[i - 1][j])
+                let length2 = (j === 0) ? 0 : (matrix[i][j - 1])
+                matrix[i][j] = Math.max(length1, length2);
+            }
+        }
+    }
+    return matrix[text1.length - 1][text2.length - 1];
 }
 
 
-
-
-
-
-// Below is pure recursion approach, O(2^n)
+// Below is pure recursion approach, O(2^n) time complexity
 const longestCommonSubsequenceRecur = (text1, text2) => {
     return LCSHelper(text1, text2, 0, 0, 0);
 }
